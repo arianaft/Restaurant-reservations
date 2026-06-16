aa0423e6690a7c1e53648b5c03b145cd95651432
Ariana Toscano Villalba <4darianatoscano@gmail.com>
2026-06-16 12:43:24 +0200

    feat: Creada clase Costumer

---
+++ restaurant-reservations/src/main/java/com/ariana/restaurant_reservations/model/Customer.java
@@ -0,0 +1,22 @@
+package com.ariana.restaurant_reservations.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name= "customers")
+@Data
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false , unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String phone;
+}
