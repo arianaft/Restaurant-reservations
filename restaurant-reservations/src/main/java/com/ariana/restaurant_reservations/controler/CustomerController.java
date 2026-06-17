5e813a5747cf552818f3024c3fc65e4e4a89d59f
Ariana Toscano Villalba <4darianatoscano@gmail.com>
2026-06-17 18:14:07 +0200

    feat: añadidos Endpoint POST Y DELETE  de Customer

--- restaurant-reservations/src/main/java/com/ariana/restaurant_reservations/controler/CustomerController.java
+++ restaurant-reservations/src/main/java/com/ariana/restaurant_reservations/controler/CustomerController.java
@@ -3,10 +3,9 @@ package com.ariana.restaurant_reservations.controler;
 import com.ariana.restaurant_reservations.model.Customer;
 import com.ariana.restaurant_reservations.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,12 +17,31 @@ public class CustomerController {
     @Autowired
     private CustomerService customerService;
 
-    //Endpoint GET
+    //Endpoint GET devuelve todos los clientes
     @GetMapping
     public ResponseEntity<List<Customer>> getAllCustomers(){
         return ResponseEntity.ok(customerService.getAllCustomers());
     }
 
+    //Endpont GET busca el cliente por id
+    @GetMapping("/{id}")
+    public ResponseEntity<Customer> getCustomerById(@PathVariable Long id){
+        return  customerService.getCustomerById(id)
+                .map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
+    //Endpoint POST
+    @PostMapping
+    public ResponseEntity<Customer> createCustomer(@RequestBody Customer customer){
+        return ResponseEntity.status(HttpStatus.CREATED).body(customerService.createCustomer(customer));
+    }
+
+    //Endpoint DELETE
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCustomer(@PathVariable Long id){
+        customerService.deleteCustomer(id);
+        return ResponseEntity.noContent().build();
+    }
 
 
 }
