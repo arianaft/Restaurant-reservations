7211fed0fed4258721b333ccdd9f580ff889f4b8
Ariana Toscano Villalba <4darianatoscano@gmail.com>
2026-06-17 17:43:12 +0200

    feat: Endpoint GET de CustomerController

--- restaurant-reservations/src/main/java/com/ariana/restaurant_reservations/controler/CustomerController.java
+++ restaurant-reservations/src/main/java/com/ariana/restaurant_reservations/controler/CustomerController.java
@@ -1,9 +1,14 @@
 package com.ariana.restaurant_reservations.controler;
 
+import com.ariana.restaurant_reservations.model.Customer;
 import com.ariana.restaurant_reservations.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 @RestController
@@ -13,6 +18,11 @@ public class CustomerController {
     @Autowired
     private CustomerService customerService;
 
+    //Endpoint GET
+    @GetMapping
+    public ResponseEntity<List<Customer>> getAllCustomers(){
+        return ResponseEntity.ok(customerService.getAllCustomers());
+    }
 
 
 
