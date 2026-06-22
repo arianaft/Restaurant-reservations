d3a3efe1004bcd53303a2facf33f766c900c3607
Ariana Toscano Villalba <4darianatoscano@gmail.com>
2026-06-22 20:18:19 +0200

    añadidas excepciones

---
+++ restaurant-reservations/src/main/java/com/ariana/restaurant_reservations/exception/ReservationConflictException.java
@@ -0,0 +1,11 @@
+package com.ariana.restaurant_reservations.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
+public class ReservationConflictException extends RuntimeException{
+    public ReservationConflictException(String message){
+        super(message);
+    }
+}
